c3678be558e49c66446d686bb35d6801b610bbff
Jahidul Islam <jahidul479@gmail.com>
2019-05-10 19:08:24 +0600

    start from scratch

---
+++ myexample.py
@@ -0,0 +1,12 @@
+print('It\'s worked')
+print('It\'s worked')
+name = "its good to see a great platform for learning python wow just love it like as a novice"
+print(name)
+name = ['jahid', 'kamal', "saifur","Fahim", "Minar"]
+
+for i in name:
+    print(i)
+
+a = 7
+print('This is {}'.format(a))
+print('This is {a}'.format(a="cool"))
