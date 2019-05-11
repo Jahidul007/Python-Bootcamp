14577ecf9be6efbb58747d72e48bee695f31a881
Jahidul Islam <jahidul479@gmail.com>
2019-05-11 13:21:21 +0600

    start from scratch

---
+++ tuples.py
@@ -0,0 +1,18 @@
+tuples1 = (1,2,3,4)
+print(tuples1)
+my_list = [1,2,3,4]
+
+# data type
+print(type(tuples1))
+print(type(my_list))
+print(len(tuples1))
+
+# indexing
+t = ('one', 2)
+print(t[-1])
+# count
+t = ('a','b','a')
+t.count('a')
+print(t.count('a'))
+print(t.index('b'))
+# tuple does not support item assignment
