079ff4295ae656b4729e00a951c3e101d131c610
Jahidul Islam <jahidul479@gmail.com>
2019-05-29 14:52:14 +0600

    oop

--- nameOfClass.py
+++ nameOfClass.py
@@ -1,8 +1,15 @@
 class Sample():
-    def __init__(self, breed):
-        self.breed = breed
+    def __init__(self, my_breed, name , spots):
+        #attributes
+        # we take in the argument
+        # Assign it using self.attribute_name
+        self.breed = my_breed
+        self.name = name
 
-my_dog = Sample(breed= 'Lab')
+        # Expect boolean True/False
+        self.spots = spots
+
+my_dog = Sample(my_breed= 'Lab', name= 'Sammy', spots=False)
 
 print(type(my_dog))
-print(my_dog.breed)
+print(my_dog.name)
