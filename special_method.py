eedf46b776d1782f61158e45fa46ecb17fcfeec3
Jahidul Islam <jahidul479@gmail.com>
2019-05-30 03:53:33 +0600

    oop

--- special_method.py
+++ special_method.py
@@ -1,12 +1,16 @@
-myList = [1,2,3]
+myList = [1, 2, 3]
 print(len(myList))
+
 
 class Sample():
     pass
+
+
 mysample = Sample()
 
 print(mysample)
 print(myList)
+
 
 class Book():
 
@@ -14,9 +18,21 @@ class Book():
         self.title = title
         self.author = author
         self.pages = pages
+
     def __str__(self):
         return f"{self.title} by {self.author}"
 
+    def __len__(self):
+        return self.pages
+
+    def __del__(self):
+        print("A book object has been deleted")
+
+
 b = Book('Python from scratch', 'O\'relly', 500)
-print(b)
+print(len(b))
 print(str(b))
+
+# delete from the memory
+#del b
+#print(b)
