b6a20cfcf0f822d12ad643f0137ce3d1ba5e19b1
Jahidul Islam <jahidul479@gmail.com>
2019-06-13 12:24:34 +0600

    generator

--- generators.py
+++ generators.py
@@ -1,7 +1,40 @@
+'''
 def create_cubes(n):
-    result = []
+    #result = []
 
     for x in range(n):
-        result.append(x**3)
-    return result
-print(create_cubes(5))
+        yield  x**3
+       # result.append(x**3)
+    #return result
+for x in create_cubes(10005):
+    print(x)
+'''
+
+def gen_fibon(n):
+
+    a = 1;
+    b = 1
+    for i in range(n):
+        yield a
+        a, b = b, a+b
+
+for number in gen_fibon(10):
+    print(number)
+
+def simple_gen():
+    for x in range(3):
+        yield x
+
+for number in simple_gen():
+    print(number)
+
+g = simple_gen()
+
+print(next(g))
+print(next(g))
+print(next(g))
+
+s = 'hello'
+
+for letter in s:
+    print(letter)
