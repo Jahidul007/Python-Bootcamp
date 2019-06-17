2859acd95b0f3c9c428d77a5d8d8583931f9bf0a
Jahidul Islam <jahidul479@gmail.com>
2019-06-18 02:19:14 +0600

    module

--- modules.py
+++ modules.py
@@ -18,3 +18,30 @@ for item in d:
 
 d = defaultdict(lambda: 0)
 print(d['one'])
+
+d = {}
+
+d['a'] = 1
+d['b'] = 2
+d['c'] = 3
+d['d'] = 4
+d['e'] = 5
+
+print(d)
+'''
+for k,v in d.items():
+    print(k, v)
+'''
+
+
+from collections import OrderedDict
+
+d['a'] = 1
+d['b'] = 2
+d['c'] = 3
+d['d'] = 4
+d['e'] = 5
+
+
+for k, v in d.items():
+    print(k,v)
