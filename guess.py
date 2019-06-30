7c513ab4cf33ebb62ce58d984c5eb80aefcfa85e
Jahidul Islam <jahidul479@gmail.com>
2019-06-30 18:21:08 +0600

    nt

--- guess.py
+++ guess.py
@@ -4,3 +4,26 @@ print("HELLO. What is your name")
 name = input()
 print("well, "+ name +' I am thinking a number between 1 and 10')
 secret_number = random.randint(1, 20)
+
+for guessTaken in range(1, 7):
+    print('Take a guess....')
+    guess = int(input())
+
+    if guess <secret_number:
+        print('Your guess is too low')
+    elif guess> secret_number:
+        print('Guess is too high')
+    else:
+        break;
+if guess == secret_number:
+    print('Good job, ' + name + '! you guessed my number in ' + str(guessTaken) + ' guesses!')
+
+
+
+
+
+
+
+print('You took ' + str(guessTaken) + ' guessses')
+
+
