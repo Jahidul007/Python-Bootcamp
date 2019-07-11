fc608bd26b37ccf045e4e6bbf432ac52ee77083d
Jahidul Islam <jahidul479@gmail.com>
2019-07-11 23:30:14 +0600

    re

---
+++ files.py
@@ -0,0 +1,12 @@
+path = r'c:\user\eggs.png'
+path = 'c:\\user\\eggs.png'
+print(path)
+
+import os
+path = os.path.join('folder1','folder2', 'folder3')
+print(path)
+
+# current working directory
+print(os.getcwd())
+
+
