99e0a3a44f1984c0e5fbfcdeaf0f51efd557ba1b
Jahidul Islam <jahidul479@gmail.com>
2019-07-26 13:14:22 +0600

    word file reade/write

--- fileWord.py
+++ fileWord.py
@@ -4,4 +4,23 @@ import os
 
 variable = docx.Document(r"C:\Users\Asus\Desktop\new.docx")
 
-print(variable)
+
+print(variable.paragraphs[0].text)
+p = variable.paragraphs[0];
+
+p.runs[0].bold
+p.runs[0].italic
+p.runs[0].underline = True
+
+variable.save(r"C:\Users\Asus\Desktop\new.docx")
+
+def getText(fileName):
+    doc = docx.Document(fileName)
+    fullText = [];
+
+    for para in doc.paragraphs:
+        fullText.append(para.text)
+    return '\n'.join(fullText)
+
+print(getText(r"C:\Users\Asus\Desktop\new.docx"))
+
