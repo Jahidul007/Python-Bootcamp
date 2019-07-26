c381278363c0271e2663949648a6484a4adf16c7
Jahidul Islam <jahidul479@gmail.com>
2019-07-26 12:40:22 +0600

    pdf reader

--- fileWord.py
+++ fileWord.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import os
 
-variable = pd.read_csv(r"C:\Users\Asus\Desktop\NER.csv")
+variable = pd.read_csv(r"C:\Users\Asus\Desktop\new.docx")
 vh = variable.head()
 print(vh)
