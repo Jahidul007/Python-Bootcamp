1490230356508e09c6285e027bd6a1139f461747
Jahidul Islam <jahidul479@gmail.com>
2019-10-04 03:27:07 +0600

    oop

---
+++ oop/class_attribute.py
@@ -0,0 +1,11 @@
+class your_class(object):
+    clsasy = 10
+
+    def set_value(self):
+        self.insty = 100
+
+class_object = your_class()
+
+class_object.set_value()
+print(class_object.clsasy)
+print(class_object.insty)
