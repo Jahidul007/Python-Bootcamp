a8bdc217e0cba0c7449261971f4145f3c9126f94
Jahidul Islam <jahidul479@gmail.com>
2019-10-06 21:03:30 +0600

    oop

--- oop/with_context.py
+++ oop/with_context.py
@@ -3,11 +3,13 @@ class MyClass(object):
     def __enter__(self):
         print('we have entered "with"')
         return self
+
     def __exit__(self, exc_type, exc_val, exc_tb):
         print('we are leaving "with"')
 
     def sayhi(self):
-        print('Hi! instance is %s' %(id(self)))
+        print('Hi! instance is %s' % (id(self)))
+
 
 with MyClass() as cc:
     cc.sayhi()
