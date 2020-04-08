add11912db7d03306e6c2d3fe6a10f15c8a94cdb
Jahidul Islam <jahidul479@gmail.com>
2020-04-08 22:22:14 +0600

    commit

--- maximum_sum_contiguous_subarray.py
+++ maximum_sum_contiguous_subarray.py
@@ -1,16 +1,14 @@
 def max_sum(arr):
     n = len(arr)
-    max = 0
+    max = arr[0]
+    current_sum = 0
     for i in range(0, n):
-        if arr[i] > max:
-            max = arr[i]
-
-        current_sum = arr[i]
-        for j in range(i + 1, n):
-            current_sum += arr[j]
-            if current_sum > max:
-                max = current_sum
+        current_sum += arr[i]
+        if max < current_sum:
+            max = current_sum
+        if current_sum < 0:
+            current_sum = 0
     return max
 
 
-print(max_sum([-1, -2, 1, 3, 2, -5, 4]))
+print(max_sum([-2, -1, 2, 1, 3, -5, 4]))
