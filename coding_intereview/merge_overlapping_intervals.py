168981809416fd4a3f0d32d3cb94590def173979
Jahidul Islam <jahidul479@gmail.com>
2020-04-10 23:52:54 +0600

    commit

--- coding_intereview/merge_overlapping_intervals.py
+++ coding_intereview/merge_overlapping_intervals.py
@@ -2,18 +2,18 @@ def merge(intervals):
     intervals.sort()
     n = len(intervals)
     new_intervals = []
-    new_intervals.append(intervals[0])
+    if n != 0:
+        new_intervals.append(intervals[0])
 
-    for i in range(1, n):
-        start, end = new_intervals[-1]
+        for i in range(1, n):
+            start, end = new_intervals[-1]
 
-    if intervals[i][0] > end:
-        new_intervals.append(intervals[i])
-    elif intervals[i][1] > end:
-        end = intervals[i][1]
-        new_intervals[-1] = (start, end)
-
+            if intervals[i][0] > end:
+                new_intervals.append(intervals[i])
+            elif intervals[i][1] > end:
+                end = intervals[i][1]
+                new_intervals[-1] = (start, end)
     return new_intervals
 
 
-print(merge([(1, 3), (8, 10), (2, 6), (15, 18)]))
+print(merge([(1, 3), (2, 6), (8, 10), (15, 18)]))
