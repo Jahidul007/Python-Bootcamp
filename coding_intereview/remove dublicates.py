d583c243b601209c7a080b926cfb300aaccbd2f1
Jahidul Islam <jahidul479@gmail.com>
2020-04-11 13:31:18 +0600

    array

--- coding_intereview/remove dublicates.py
+++ coding_intereview/remove dublicates.py
@@ -3,6 +3,8 @@ def remove_duplicates(nums):
     unique_nums.append(nums[0])
 
     n = len(nums)
+    if n == 0:
+        return 0
     for i in range(1, n):
         if (nums[i] != nums[i - 1]):
             unique_nums.append(nums[i])
@@ -13,8 +15,7 @@ def remove_duplicates1(nums):
     unique_nums = list(set(nums))
     unique_nums.sort()
 
-    return len(unique_nums)
+    return unique_nums
 
 
-
-print(remove_duplicates1([1, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 4, 4, 4, 4, 4]))
+print(remove_duplicates1([1, 1, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 4, 4, 4, 4, 4]))
