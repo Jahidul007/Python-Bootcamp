b884808ef923b7eaf766756e2eca0fc2c92de538
Jahidul Islam <jahidul479@gmail.com>
2020-04-18 23:39:33 +0600

    array

--- coding_intereview/counting_triangle.py
+++ coding_intereview/counting_triangle.py
@@ -1,27 +1,25 @@
-def count_triangles(A):
-    if len(A) < 3:
-        return 0
+class Solution:
+    def triangleNumber(self, nums) -> int:
+        if len(nums) < 3:
+            return 0
+        nums.sort()
+        n = len(nums)
+        res = 0
+        sum = 0
+        for i in range(n - 2):
+            for j in range(i + 1, n - 1):
+                sum = nums[i] + nums[j]
+                left = j + 1
+                right = n;
+                while (left < right):
+                    mid = left + (right - left) // 2
+                    if (nums[mid] < sum):
+                        left = mid + 1
+                    else:
+                        right = mid
 
-    A.sort()
+                res += right - 1 - j
+        return res
 
-    i, j, k, = 0, 1, 2
-    count = 0
 
-    n = len(A)
-    while i < n - 2:
-        j = i + 1
-        k = i + 2
-        while j < n - 1:
-            two_sides = A[i] + A[j]
-            while k < n:
-                if two_sides <= A[k]:
-                    break
-                k += 1
-            count += k - j - 1
-
-            j += 1
-        i += 1
-
-    return count
-
-print(count_triangles([0,1,0,1]))
+print(Solution().count_triangles([0,1,0,1]))
