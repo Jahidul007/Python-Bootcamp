3f75595fe93f6455c57750d17a7a5878f7074946
Jahidul Islam <jahidul479@gmail.com>
2020-04-19 23:22:29 +0600

    stack

---
+++ coding_intereview/rudundant_braces.py
@@ -0,0 +1,16 @@
+class Solution:
+    # @param A : string
+    # @return an integer
+    def braces(self, A):
+        stack = []
+        for item in A:
+            if item in "(+-*/":
+                stack.append(item)
+            elif item in ")":
+                last_item =stack.pop()
+                if last_item == "(":
+                    return True
+                while "(" != stack.pop():
+                    continue
+        return False
+
