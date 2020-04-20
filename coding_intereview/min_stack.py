77dabccd097cd5ebd63a44cc9fd70a92ea1d3ae5
Jahidul Islam <jahidul479@gmail.com>
2020-04-21 00:39:09 +0600

    stack/queue

---
+++ coding_intereview/min_stack.py
@@ -0,0 +1,39 @@
+class MinStack:
+
+    def __init__(self):
+        """
+        initialize your data structure here.
+        """
+        self.stk = []
+        self.min_stk = []
+
+    def push(self, x: int) -> None:
+        self.stk.append(x)
+        if len(self.min_stk)== 0 or x <= self.min_stk[-1]:
+            self.min_stk.append(x)
+            print(self.min_stk[-1])
+            print(self.min_stk)
+
+    def pop(self) -> None:
+        x = self.stk.pop()
+        if x == self.min_stk[-1]:
+            self.min_stk.pop()
+
+    def top(self) -> int:
+        if len(self.stk) != 0:
+            return self.stk[-1]
+
+    def getMin(self) -> int:
+        if len(self.min_stk) != 0:
+            return self.min_stk[-1]
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(x)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()
+
+
+ls = [1,2,3,4,5,6,7,8]
+print(ls[-1])
