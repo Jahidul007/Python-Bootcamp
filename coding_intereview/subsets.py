de1e7f8bf22bbcebf3c55b161323347569624803
Jahidul Islam <jahidul479@gmail.com>
2020-04-22 17:25:22 +0600

    combinations

--- coding_intereview/subsets.py
+++ coding_intereview/subsets.py
@@ -17,6 +17,26 @@ class Solution(object):
                 li.pop()
 
         recurse(0, len(nums), [])
+        res = []
+        for i in result:
+            if i not in res:
+                res.append(i)
+        return res
+    # generate subset using a stack
+
+    def subset(self, nums):
+        if nums == []:
+            return  [[]]
+
+        result = [[]]
+
+        while len(nums):
+            temp_r = []
+            x = nums.pop()
+            for i in result:
+                temp_r.append(i + [])
+                temp_r.append(i + [x])
+            result = [item for item in temp_r]
         return result
 
-print(Solution().subsets([1,2,3]))
+print(Solution().subset([1,2,2]))
