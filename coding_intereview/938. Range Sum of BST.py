54c226efcf64a7d0a6091911fe92636af058d2a2
Jahidul Islam <jahidul479@gmail.com>
2020-05-03 03:09:29 +0600

    tree

--- coding_intereview/938. Range Sum of BST.py
+++ coding_intereview/938. Range Sum of BST.py
@@ -4,15 +4,17 @@ class TreeNode:
         self.val = val
         self.left = left
         self.right = right
+
+
 class Solution:
     def rangeSumBST(self, root: TreeNode, L: int, R: int) -> int:
-        if not root: 
+        if not root:
             return 0
         sum = 0
-        if L <= root.val <= R: 
+        if L <= root.val <= R:
             sum += root.val
-        if L < root.val: 
+        if L < root.val:
             sum += self.rangeSumBST(root.left, L, R)
-        if R > root.val: 
+        if R > root.val:
             sum += self.rangeSumBST(root.right, L, R)
-        return sum 
+        return sum
