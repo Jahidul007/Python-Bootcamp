1dd11f0c3175c4acc5e9cfb29d170cfb4da16373
Jahidul Islam <jahidul479@gmail.com>
2020-05-10 03:10:25 +0600

    array

--- coding_intereview/283. Move Zeroes.py
+++ coding_intereview/283. Move Zeroes.py
@@ -3,12 +3,12 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        indx =0
+        indx = 0
         for i in range(len(nums)):
-            if(nums[i])!=0:
-                nums[indx]=nums[i]
-                indx+=1
-        while(indx<len(nums)):
-            nums[indx]=0
-            indx+=1
+            if (nums[i]) != 0:
+                nums[indx] = nums[i]
+                indx += 1
+        while (indx < len(nums)):
+            nums[indx] = 0
+            indx += 1
         return nums
