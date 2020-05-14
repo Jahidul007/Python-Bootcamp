1a37881c71129475edf1ba0769e04f5468665223
Jahidul Islam <jahidul479@gmail.com>
2020-05-15 01:49:03 +0600

    math

---
+++ coding_intereview/836. Rectangle Overlap.py
@@ -0,0 +1,5 @@
+class Solution:
+    def isRectangleOverlap(self, rec1: List[int], rec2: List[int]) -> bool:
+        [A, B, C, D], [E, F, G, H] = rec1, rec2
+        x, y = (min(C, G) - max(A, E)), (min(D, H) - max(B, F))
+        return x > 0 and y > 0
