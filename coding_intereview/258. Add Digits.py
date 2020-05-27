b70889b57187dd5c31dee9815a23527a3f287f71
Jahidul Islam <jahidul479@gmail.com>
2020-05-28 05:16:47 +0600

    math

--- coding_intereview/258. Add Digits.py
+++ coding_intereview/258. Add Digits.py
@@ -1,7 +1,7 @@
 class Solution:
     def addDigits(self, num: int) -> int:
-        while num>=10:
+        while num >= 10:
             sum = 0;
-            sum+= num//10 + num%10
+            sum += num // 10 + num % 10
             num = sum
         return num
