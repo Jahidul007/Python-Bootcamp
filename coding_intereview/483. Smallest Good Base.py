b8486b5d238d2d0ec43ffe4fbc102656981cd901
Jahidul Islam <jahidul479@gmail.com>
2020-06-01 01:26:25 +0600

    binary search

---
+++ coding_intereview/483. Smallest Good Base.py
@@ -0,0 +1,14 @@
+class Solution:
+    def smallestGoodBase(self, n: str) -> str:
+        n = int(n)
+        m = len(bin(n)) - 2
+        while m > 0:
+            low, high = 2, n - 1
+            while low <= high:
+                mid = (low + high) // 2
+                res = (mid ** m - 1) // (mid - 1)
+                if res == n: return str(mid)
+                elif res < n: low = mid + 1
+                else: high = mid - 1
+            m -= 1
+        return str(n)
