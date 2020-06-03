510c848843a0531c31614c9862ff3701368c80fc
Jahidul Islam <jahidul479@gmail.com>
2020-06-03 23:33:10 +0600

    math

---
+++ coding_intereview/665. Non-decreasing Array.py
@@ -0,0 +1,16 @@
+class Solution:
+    def checkPossibility(self, nums: List[int]) -> bool:
+        ans = 0
+        for i in range(len(nums) - 1):
+            if nums[i] > nums[i + 1]:
+                if ans < 1:
+                    if i == 0:
+                        nums[i] = nums[i + 1]
+                    elif nums[i - 1] <= nums[i + 1]:
+                        nums[i] = nums[i - 1]
+                    else:
+                        nums[i + 1] = nums[i]
+                    ans += 1
+                else:
+                    return False
+        return True
