83832c5976eb7098af9b0dd335ac834fe252538b
Jahidul Islam <jahidul479@gmail.com>
2020-06-04 00:11:25 +0600

    math

--- coding_intereview/1015. Smallest Integer Divisible by K.py
+++ coding_intereview/1015. Smallest Integer Divisible by K.py
@@ -1,12 +1,12 @@
 class Solution:
     def smallestRepunitDivByK(self, k: int) -> int:
-        if k%2==0 or k%5==0:
+        if k % 2 == 0 or k % 5 == 0:
             return -1
         if k == 1:
             return 1
-        count =1
+        count = 1
         n = 1
-        while(n%k>0):
-            n = (n%k)*10+1
-            count +=1
+        while (n % k > 0):
+            n = (n % k) * 10 + 1
+            count += 1
         return count
