13fbfbe9572a2fa46599bcd42a1e054ff26345fe
Jahidul Islam <jahidul479@gmail.com>
2020-06-06 22:25:23 +0600

    parentheses

--- coding_intereview/1021. Remove Outermost Parentheses.py
+++ coding_intereview/1021. Remove Outermost Parentheses.py
@@ -3,12 +3,12 @@ class Solution:
         count = 0
         final = ""
         for i in S:
-            if i=="(":
-                count+=1
-                if count!=1:
-                    final+=i
+            if i == "(":
+                count += 1
+                if count != 1:
+                    final += i
             else:
-                count-=1
-                if count!=0:
+                count -= 1
+                if count != 0:
                     final += i
         return final
