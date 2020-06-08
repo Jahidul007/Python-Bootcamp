85156554c9adad7704338c8cafa175a9a23b7192
Jahidul Islam <jahidul479@gmail.com>
2020-06-09 00:20:26 +0600

    max

--- coding_intereview/485. Max Consecutive Ones.py
+++ coding_intereview/485. Max Consecutive Ones.py
@@ -4,11 +4,11 @@ class Solution:
         max1 = 0
         for i in range(len(nums)):
             if nums[i] == 1:
-                count+=1
-            if nums[i] ==0:
-                if count>max1:
-                    max1=count
+                count += 1
+            if nums[i] == 0:
+                if count > max1:
+                    max1 = count
                 count = 0
-        if count>max1:
-            max1=count
+        if count > max1:
+            max1 = count
         return max1
