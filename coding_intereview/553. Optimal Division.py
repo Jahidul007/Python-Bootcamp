02d43c7e54c950c778ee56d0060e2cc2edd39f72
Jahidul Islam <jahidul479@gmail.com>
2020-06-13 01:33:31 +0600

    math

--- coding_intereview/553. Optimal Division.py
+++ coding_intereview/553. Optimal Division.py
@@ -1,6 +1,6 @@
 class Solution:
     def optimalDivision(self, nums: List[int]) -> str:
         nums = tuple(map(str, nums))
-        if len(nums)<=2:
+        if len(nums) <= 2:
             return '/'.join(nums)
-        return nums[0]+'/(' + '/'.join(nums[1:]) + ')'
+        return nums[0] + '/(' + '/'.join(nums[1:]) + ')'
