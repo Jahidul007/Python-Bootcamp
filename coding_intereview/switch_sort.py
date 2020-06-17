21d6d5f1fbfea7b891d1e6faae18024b2eae5d02
Jahidul Islam <jahidul479@gmail.com>
2020-06-17 21:30:26 +0600

    prime

---
+++ coding_intereview/switch_sort.py
@@ -0,0 +1,18 @@
+def SwitchSort(arr):
+    swap = 0
+    visited = [False for i in range(len(arr))]
+    for i in range(len(arr)):
+        j = i
+        counter = 0
+        while not visited[j]:
+            visited[j] = True
+            j = arr[j] - 1
+            counter = counter + 1
+        if counter != 0:
+            swap = swap + (counter - 1)
+    return swap
+
+
+# keep this function call here
+arr = [1,3,4,2]
+print(SwitchSort(arr))
