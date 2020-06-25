7f7c8c3d32fde179d4dbb50b18115be5d2bcbed8
Jahidul Islam <jahidul479@gmail.com>
2020-06-25 23:46:43 +0600

    tribonacci

---
+++ coding_intereview/1137. N-th Tribonacci Number.py
@@ -0,0 +1,14 @@
+from functools import lru_cache
+
+
+class Solution:
+    @lru_cache(maxsize=1000)
+    def tribonacci(self, n: int) -> int:
+        if n == 0:
+            return 0
+        elif n == 1:
+            return 1
+        elif n == 2:
+            return 1
+        else:
+            return self.tribonacci(n - 1) + self.tribonacci(n - 2) + self.tribonacci(n - 3)
