e8fd6f293ab76a20869a2e209fba1c464e3dfb14
Jahidul Islam <jahidul479@gmail.com>
2020-07-12 22:21:20 +0600

    substring

--- coding_intereview/1513. Number of Substrings With Only 1s.py
+++ coding_intereview/1513. Number of Substrings With Only 1s.py
@@ -1,1 +1,12 @@
-
+class Solution:
+    def numSub(self, s: str) -> int:
+        cont = s.split('0')
+        cur = 0
+        res = 0
+        for i in s:
+            if i =='0':
+                cur = 0
+            else:
+                cur+=1
+                res +=cur
+        return res% (10**9+7)
