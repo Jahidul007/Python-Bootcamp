b22e4c5dc545ef80493aa7852e93917ecaa19ff3
Jahidul Islam <jahidul479@gmail.com>
2020-07-23 21:43:12 +0600

    Update 1402. Reducing Dishes.py

--- coding_intereview/1402. Reducing Dishes.py
+++ coding_intereview/1402. Reducing Dishes.py
@@ -1,2 +1,13 @@
-
+class Solution:
+    def maxSatisfaction(self, satisfaction: List[int]) -> int:
+        satisfaction.sort()
+        best,esum,total = 0,0,0
+        for x in reversed(satisfaction):
+            esum  += x
+            total += esum
+            if total>best:
+                best = total
+            elif esum<0:
+                break
+        return best
 
