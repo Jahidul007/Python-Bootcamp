b97c53af88d71d5d2e131765a0e69a907e901cea
Jahidul Islam <jahidul479@gmail.com>
2020-08-30 23:39:37 +0600

    iterative

---
+++ coding_intereview/1566. Detect Pattern of Length M Repeated K or More Times.py
@@ -0,0 +1,11 @@
+class Solution:
+    def containsPattern(self, arr: List[int], m: int, k: int) -> bool:
+        i = 0
+        while i <= len(arr)-1:
+            p = arr[i:i+m]
+            if p * k == arr[i:i+m*k]:
+                return True
+
+            i += 1
+
+        return False
