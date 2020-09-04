6350250035613e98411b3fe780af55bfe4ded0ef
Jahidul Islam <jahidul479@gmail.com>
2020-09-04 23:05:03 +0600

    list

--- coding_intereview/1557. Minimum Number of Vertices to Reach All Nodes.py
+++ coding_intereview/1557. Minimum Number of Vertices to Reach All Nodes.py
@@ -1,3 +1,7 @@
-
-
-
+class Solution:
+    def findSmallestSetOfVertices(self, n: int, edges: List[List[int]]) -> List[int]:
+        ns = set(range(n))
+        for i,j in edges:
+            if j in ns: ns.remove(j)
+                 
+        return list(ns)
