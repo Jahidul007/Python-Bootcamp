dd3c918bab4ec1cfc7968dbc657df727b3d6360d
Jahidul Islam <jahidul479@gmail.com>
2020-09-10 22:18:59 +0600

    matrix

--- coding_intereview/1572. Matrix Diagonal Sum.py
+++ coding_intereview/1572. Matrix Diagonal Sum.py
@@ -1,1 +1,9 @@
-
+class Solution:
+    def diagonalSum(self, mat: List[List[int]]) -> int:
+        total = 0
+        for i in range(len(mat)):
+            total += mat[i][i]  # elements from the primary diagonal
+            total += mat[i][len(mat)-1-i]  # secondary diagonal
+        if len(mat) % 2 == 0:
+            return total
+        return total - mat[len(mat)//2][len(mat)//2]
