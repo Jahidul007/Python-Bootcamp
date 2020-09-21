52b96f5ab2473971e0390b08d73b81a0bca5b9d5
jahiduloo7 <jahidul479@gmail.com>
2020-09-21 13:02:29 +0600

    commit

---
+++ coding_intereview/1592. Rearrange Spaces Between Words.py
@@ -0,0 +1,11 @@
+class Solution:
+    def reorderSpaces(text: str) -> str:
+        spaces = text.count(' ')
+        words = text.split()
+        if len(words) == 1:
+            return words[0] + (' ' * spaces)
+        sep_count = len(words) - 1
+        spaces_between_words = spaces // sep_count
+        spaces_in_the_end = spaces % sep_count
+        return (' ' * spaces_between_words).join(words) + (' ' * spaces_in_the_end)
+print(Solution.reorderSpaces(" this is a sentencce"))
