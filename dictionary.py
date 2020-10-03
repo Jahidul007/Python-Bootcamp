523586a71a82c17f1663cfb9060abcbefca57d3d
Purusharth Malik <56820986+purusharthmalik@users.noreply.github.com>
2020-10-03 22:17:30 +0530

    Update dictionary.py
    Added all the dictionary methods for ready reference.

--- dictionary.py
+++ dictionary.py
@@ -22,3 +22,19 @@ print(d.values())
 num1 = 100
 num2 = 100
 print("Sum", num1+num2)
+
+
+#Methods that are available in dictionaries
+
+sample_dict = {1:'a', 2:'b', 3:'c', 4:'d'}
+sample_dict.clear()                       #Clears the dictionary without deleting it
+sample_dict.copy()                        #Copies the dictionary to a new memory location
+sample_dict.fromkeys()                    #Returns a dictionary with the specified keys and value
+sample_dict.keys()                        #Returns a list containing the dictionary's keys
+sample_dict.values()                      #Returns a list of all the values in the dictionary
+sample_dict.get()                         #Returns the value of the specified key
+sample_dict.items()                       #Returns a list containing a tuple for each key value pair 
+sample_dict.pop()                         #Removes the element with the specified key
+sample_dict.popitem()                     #Removes the last inserted key-value pair
+sample_dict.update()                      #Updates the dictionary with the specified key-value pairs
+sample_dict.setdefault()                  #Returns the value of the specified key. If the key does not exist: insert the key, with the specified value
