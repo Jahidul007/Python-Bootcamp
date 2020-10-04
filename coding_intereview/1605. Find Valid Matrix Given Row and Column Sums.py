a2c313369f95752eaa287c47051980b3bda1f112
Jahidul Islam <jahidul479@gmail.com>
2020-10-05 00:43:50 +0600

    commit

--- coding_intereview/1605. Find Valid Matrix Given Row and Column Sums.py
+++ coding_intereview/1605. Find Valid Matrix Given Row and Column Sums.py
@@ -1,1 +1,13 @@
-
+class Solution:
+    def restoreMatrix(self, rowSum: List[int], colSum: List[int]) -> List[List[int]]:
+        row = len(rowSum)
+        col = len(colSum)
+        
+        mat = [[0] * col for i in range(row)]
+        
+        for i in range (row):
+            for j in range (col):
+                mat[i][j] = min(rowSum[i], colSum[j])
+                rowSum[i] -= mat[i][j]
+                colSum[j] -= mat[i][j]
+        return mat
