c267e860de0e08cfea052d6ebf35c892813b30ee
Jahidul Islam <jahidul479@gmail.com>
2020-10-08 11:52:36 +0600

    tree traversal

---
+++ coding_intereview/102. Binary Tree Level Order Traversal.py
@@ -0,0 +1,19 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def levelOrder(self, root: TreeNode) -> List[List[int]]:
+        
+        if root == None:
+            return []
+        q = [root]
+        lis = []
+        while q:
+            l = [node.val for node in q]
+            lis.append(l)
+            q = [child for node in q for child in (node.left, node.right) if child]
+        
+        return lis
